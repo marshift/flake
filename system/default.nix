77987b4028de0adec8ab4a068a53e436a1a5b585
marshift <marshift@riseup.net>
2024-08-19 17:37:52 +0100

    [System] Limit number of generations in bootloaders

--- system/default.nix
+++ system/default.nix
@@ -13,6 +13,15 @@
     extraGroups = [ "wheel" ];
   };
 
+  # Don't clog my boot menu please
+  boot.loader = {
+    systemd-boot.configurationLimit = 3;
+    grub.configurationLimit = 3;
+  };
+
+  # Make more than 1/5 of the hardware work
+  hardware.enableAllFirmware = true;
+
   # Don't touch - or at least do some reading first.
   system.stateVersion = "24.05";
 }
