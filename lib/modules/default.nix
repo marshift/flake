2f2efcf607fbe07a5d67bd99048010c0250b43c9
marshift <marshift@riseup.net>
2024-08-24 17:42:44 +0100

    [Lib > Modules] Add support for additional options

--- lib/modules/default.nix
+++ lib/modules/default.nix
@@ -2,12 +2,13 @@
 
 {
   ## Create a toggleable module
-  mkModule = { config, path, output }: let
+  mkModule = { config, path, output, extraOptions ? {} }: let
     name = lib.last path;
-    fullPath = path ++ ["enable"];
+    statePath = path ++ ["enable"];
+    mergedOptions = lib.mergeAttrs { enable = lib.mkEnableOption name; } extraOptions;
   in
   {
-    options = lib.setAttrByPath fullPath (lib.mkEnableOption name);
-    config = lib.mkIf (lib.attrByPath fullPath false config) output;
+    options = lib.setAttrByPath path mergedOptions;
+    config = lib.mkIf (lib.attrByPath statePath false config) output;
   };
 }
