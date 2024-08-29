8e41598b2c9e73f4c4cce5e98a19de226bef1ea4
marshift <marshift@riseup.net>
2024-08-29 19:11:01 +0100

    [Homes > Vlei] Copy config from cedar, for now

--- homes/x86_64-linux/marsh@vlei/default.nix
+++ homes/x86_64-linux/marsh@vlei/default.nix
@@ -7,7 +7,29 @@
       gh.enable = true;
     };
     desktop = {
-      hyprland.enable = true;
+      firefox.enable = true;
+      fuzzel.enable = true;
+      foot.enable = true;
+      niri = {
+        enable = true;
+        keyboardLayout = "gb";
+        outputs = {
+          "DP-1" = {
+            mode = {
+              width = 3440;
+              height = 1440;
+              refresh = 144.0;
+            };
+          };
+          "DP-2" = {
+            mode = {
+              width = 1920;
+              height = 1080;
+              refresh = 60.0;
+            };
+          };
+        };
+      };
     };
   };
 
