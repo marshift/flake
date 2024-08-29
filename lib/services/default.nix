253eafb50a871dd247dfd55ec888384becf213b6
marshift <marshift@riseup.net>
2024-08-29 21:26:56 +0100

    [Lib > Services] Support ExecReload and KillMode passthrough

--- lib/services/default.nix
+++ lib/services/default.nix
@@ -1,17 +1,26 @@
 { lib, ... }:
 
 {
-  mkGraphicalService = { description, targetService, execStart }: {
+  # TODO: This is kinda awful and debatably unnecessary
+  mkGraphicalService = { description, targetService, execStart, execReload ? null, killMode ? null }: {
     Unit = {
       Description = description;
       PartOf = [ "graphical-session.target" ];
       Requires = [ targetService ];
       After = [ targetService ];
     };
-    Service = {
-      ExecStart = execStart;
-      Restart = "on-failure";
-    };
+    Service = lib.mergeAttrsList [
+      {
+        ExecStart = execStart;
+        Restart = "on-failure";
+      }
+      (lib.optionalAttrs (execReload != null) {
+        ExecReload = execReload;
+      })
+      (lib.optionalAttrs (killMode != null) {
+        KillMode = killMode;
+      })
+    ];
     Install = {
       WantedBy = [ targetService ];
     };
