7dd3140a17e3a0a83751fd3a719e0f6ca92d88bd
marshift <marshift@riseup.net>
2024-08-30 02:52:02 +0100

    [Modules > Niri] Rethink keybinds a bit

--- modules/home/desktop/niri/default.nix
+++ modules/home/desktop/niri/default.nix
@@ -73,37 +73,45 @@ lib.habitat.mkModule {
             desktopPrograms = config.habitat.home.desktop;
           in
           lib.mergeAttrsList [
+            # TODO: These are *still* not laptop compatible
+            # Also, they assume I'll never have monitors on a vertical axis.
+            # ...Which I probably won't..
             {
-              "Mod+Shift+Slash".action = show-hotkey-overlay;
-              "Mod+Q".action = close-window;
-              "Mod+Left".action = focus-column-left;
+              "Mod+Left".action = focus-column-or-monitor-left;
               "Mod+Down".action = focus-window-down;
               "Mod+Up".action = focus-window-up;
-              "Mod+Right".action = focus-column-right;
-              "Mod+H".action = focus-column-left;
-              "Mod+J".action = focus-window-down;
-              "Mod+K".action = focus-window-up;
-              "Mod+L".action = focus-column-right;
-              "Mod+Ctrl+Left".action = move-column-left;
+              "Mod+Right".action = focus-column-or-monitor-right;
+              "Mod+Ctrl+Left".action = move-column-left-or-to-monitor-left;
               "Mod+Ctrl+Down".action = move-window-down;
               "Mod+Ctrl+Up".action = move-window-up;
-              "Mod+Ctrl+Right".action = move-column-right;
-              "Mod+Ctrl+H".action = move-column-left;
-              "Mod+Ctrl+J".action = move-window-down;
-              "Mod+Ctrl+K".action = move-window-up;
-              "Mod+Ctrl+L".action = move-column-right;
-              "Mod+Tab".action = focus-window-down-or-column-right;
-              "Mod+Shift+Tab".action = focus-window-up-or-column-left;
+              "Mod+Ctrl+Right".action = move-column-right-or-to-monitor-right;
               "Mod+Home".action = focus-column-first;
               "Mod+End".action = focus-column-last;
               "Mod+Ctrl+Home".action = move-column-to-first;
               "Mod+Ctrl+End".action = move-column-to-last;
               "Mod+Page_Down".action = focus-workspace-down;
               "Mod+Page_Up".action = focus-workspace-up;
-              "Mod+Ctrl+Page_Down".action = move-column-to-workspace-down;
-              "Mod+Ctrl+Page_Up".action = move-column-to-workspace-up;
               "Mod+Shift+Page_Down".action = move-workspace-down;
               "Mod+Shift+Page_Up".action = move-workspace-up;
+              "Mod+Ctrl+Page_Down".action = move-column-to-workspace-down;
+              "Mod+Ctrl+Page_Up".action = move-column-to-workspace-up;
+              "Mod+Comma".action = consume-or-expel-window-left;
+              "Mod+Period".action = consume-or-expel-window-right;
+              "Mod+Minus".action = set-column-width "-10%";
+              "Mod+Equal".action = set-column-width "+10%";
+              "Mod+Shift+Minus".action = set-window-height "-10%";
+              "Mod+Shift+Equal".action = set-window-height "+10%";
+              "Mod+R".action = switch-preset-column-width;
+              "Mod+F".action = maximize-column;
+              "Mod+Shift+F".action = fullscreen-window;
+              "Mod+C".action = center-column;
+              "Mod+Q".action = close-window;
+              "Mod+Shift+E".action = quit;
+              "Mod+Shift+P".action = power-off-monitors;
+            }
+            # Scroll wheel
+            # TODO: Trackpad?
+            {
               "Mod+WheelScrollDown" = {
                 cooldown-ms = 150;
                 action = focus-workspace-down;
@@ -120,29 +128,22 @@ lib.habitat.mkModule {
                 cooldown-ms = 150;
                 action = move-column-to-workspace-up;
               };
-              "Mod+Shift+WheelScrollDown".action = focus-column-right;
-              "Mod+Shift+WheelScrollUp".action = focus-column-left;
-              "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right;
-              "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left;
-              "Mod+Comma".action = consume-window-into-column;
-              "Mod+Period".action = expel-window-from-column;
-              "Mod+R".action = switch-preset-column-width;
-              "Mod+F".action = maximize-column;
-              "Mod+Shift+F".action = fullscreen-window;
-              "Mod+C".action = center-column;
-              "Mod+Minus".action = set-column-width "-10%";
-              "Mod+Equal".action = set-column-width "+10%";
-              "Mod+Shift+Minus".action = set-window-height "-10%";
-              "Mod+Shift+Equal".action = set-window-height "+10%";
+              "Mod+Shift+WheelScrollDown".action = focus-column-or-monitor-right;
+              "Mod+Shift+WheelScrollUp".action = focus-column-or-monitor-left;
+              "Mod+Ctrl+Shift+WheelScrollDown".action = move-column-right-or-to-monitor-right;
+              "Mod+Ctrl+Shift+WheelScrollUp".action = move-column-left-or-to-monitor-left;
+            }
+            # Screenshots
+            {
               "Print".action = screenshot;
               "Ctrl+Print".action = screenshot-screen;
               "Alt+Print".action = screenshot-window;
-              "Mod+Shift+E".action = quit;
-              "Mod+Shift+P".action = power-off-monitors;
             }
+            # Foot terminal support
             (lib.optionalAttrs desktopPrograms.foot.enable {
               "Mod+Return".action.spawn = "${lib.getExe config.programs.foot.package}";
             })
+            # Fuzzel support
             (lib.optionalAttrs desktopPrograms.fuzzel.enable {
               "Mod+D".action.spawn = "${lib.getExe config.programs.fuzzel.package}";
             })
