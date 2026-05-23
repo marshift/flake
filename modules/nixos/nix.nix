91a784b1a153eeaae705a5a4d73166b8b1f55a30
marshift <marshift@riseup.net>
2026-05-23 15:37:55 +0100

    modules/nixos/nix: switch to lix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -5,9 +5,12 @@
   environment.systemPackages = [ pkgs.gitMinimal ]; # Flakes need git, so put it here even if unconfigured
 
   nixpkgs.config.allowUnfree = true;
-  nix.settings = {
-    extra-substituters = [ "https://nixos-apple-silicon.cachix.org" ];
-    extra-trusted-public-keys = [ "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20=" ];
-    experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    package = pkgs.lixPackageSets.stable.lix; # Make the configuration delicious
+    settings = {
+      extra-substituters = [ "https://nixos-apple-silicon.cachix.org" ];
+      extra-trusted-public-keys = [ "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20=" ];
+      experimental-features = [ "nix-command" "flakes" ];
+    };
   };
 }
